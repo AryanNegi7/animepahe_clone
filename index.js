0249602bee78a748dad4661d1ce602aab8223fbf
aryan <aryan6396596910negi@gmail.com>
2022-03-22 00:31:52 +0530

    2nd commiat

---
+++ index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    var y = document.getElementById("search");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+
+    if (y.style.display === "block") {
+        y.style.display = "none";
+      } else {
+        y.style.display = "block";
+      }
+  }
